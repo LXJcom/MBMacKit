792c38b9cd2e62227de96610d8d65e4d8dabc4da
lxjcold <lxjcold@163.com>
2024-08-04 15:15:23 +0800

    0.0.3

---
+++ MBMacKit/NavigationViewController.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationViewController.swift
+//  MBMacKit
+//
+//  Created by xiaojie li on 2024/8/4.
+//
+
+import Cocoa
+
+class NavigationViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
