76cd39fb525a1822c89b16fca7677e75fec79d58
lxjcold <lxjcold@163.com>
2024-08-04 20:46:27 +0800

    新建工程

---
+++ MBMacKit/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MBMacKit
+//
+//  Created by xiaojie li on 2024/8/4.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
